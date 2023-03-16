60116c8a9d976bcf0a1497d2dbe2239a7eaf50e6
abinphilip-1 <tabinphilip@gmail.com>
2023-03-16 20:29:08 +0000

    updated server url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) =>{
 
     // fetch data from server
 
-    const response = await fetch('http://localhost:5001',{
+    const response = await fetch('https://chatgpt-xn7y.onrender.com/',{
         method: 'POST',
         headers: {
             'Content-Type':'application/json'
